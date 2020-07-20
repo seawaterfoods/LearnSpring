d47ae36759ea8fb49c60cdfd869f976850e03243
seawaterfood <99390124@ss25.mcu.edu.tw>
2020-07-20 10:12:05 +0800

    Design Pattern 新增Singleton Pattern中使用的範例。

--- src/main/java/com/joe/designpattern/factorypattern/factory/store/PizzaStore.java
+++ src/main/java/com/joe/designpattern/factorypattern/factory/store/PizzaStore.java
@@ -6,14 +6,14 @@ public abstract class PizzaStore {
     public abstract Pizza createPizza(String item);
 
     public Pizza orderPizza(String type){
-    Pizza pizza= createPizza(type);
+        Pizza pizza= createPizza(type);
 
-        System.out.println("--- Making a " + pizza.getName() + " ---");
-    pizza.prepare();
-    pizza.bake();
-    pizza.cut();
-    pizza.box();
-    return pizza;
+            System.out.println("--- Making a " + pizza.getName() + " ---");
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
     }
 
 }

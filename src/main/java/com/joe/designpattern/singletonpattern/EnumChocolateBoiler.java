b590d57ca0d695c1d5b184897d68b8eddba1ceea
seawaterfood <99390124@ss25.mcu.edu.tw>
2020-07-20 10:48:20 +0800

    Design Pattern 新增Singleton Pattern中使用Enum單例模式與新增測試。

--- src/main/java/com/joe/designpattern/singletonpattern/EnumChocolateBoiler.java
+++ src/main/java/com/joe/designpattern/singletonpattern/EnumChocolateBoiler.java
@@ -1,69 +1,57 @@
 package com.joe.designpattern.singletonpattern;
 
-
-public class ChocolateBoiler {
+public enum EnumChocolateBoiler {
+    INSTANCE;
     private boolean empty;
     private boolean boiled;
-    private static ChocolateBoiler uniqueInstance;
 
-    public static ChocolateBoiler getInstance() {
-        if (uniqueInstance == null) {
-            synchronized (ChocolateBoiler.class) {
-                if (uniqueInstance == null) {
-                    uniqueInstance = new ChocolateBoiler();
-                    return uniqueInstance;
-                }
-            }
-        }
-        return uniqueInstance;
-    }
 
-    private ChocolateBoiler() {
+    private EnumChocolateBoiler(){
 //        代碼開始時鍋爐是空的
         empty = true;
         boiled = false;
+        System.out.println("Get singleton!");
     }
 
-    public void fill() {
+    public void fill(){
 //        在鍋爐內填入原料時，鍋爐必須是空的，一但填入原料，
 //        就把empty跟boiled標誌設置好。
-        if (isEmpty()) {
+        if (isEmpty()){
             empty = false;
             boiled = false;
             //在鍋爐中填滿巧克力和牛奶的混和物
         }
     }
 
-
-    public void boil() {
+    public void boil(){
 //      煮混合物時，鍋爐必須是滿的，並且是沒煮過的，
 //      一但煮沸後，就把boiled標誌設為true。
-        if (!isEmpty() && !isBoiled()) {
+        if (!isEmpty()&&!isBoiled()){
 //            將爐內物煮沸
             boiled = true;
         }
     }
 
-    public void drain() {
+    public void drain(){
 //        鍋爐排出時，必須是滿的(不可以是空的)而且是煮過的，
 //        排出完畢後，把empty標誌設回true。
-        if (!isEmpty() && isBoiled()) {
+        if (!isEmpty() && isBoiled()){
 //            排出煮沸的巧克力和牛奶
-            empty = true;
+            empty =true;
         }
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return empty;
     }
 
-    public boolean isBoiled() {
+    public boolean isBoiled(){
         return boiled;
     }
 
     @Override
     public String toString() {
-        return "ChocolateBoiler{" +
+        return "EnumChocolateBoiler{" +
                 "empty=" + empty +
                 ", boiled=" + boiled +
                 '}';
